2fe52598fc174868aeef1ada3a8d3ef91f33aa0f
gbyorn <byorn.gosu@gmail.com>
2020-04-11 17:49:06 +0300

    Add test and description for list

--- HomeWork2/test_list.py
+++ HomeWork2/test_list.py
@@ -3,17 +3,26 @@ class TestList:
         assert isinstance(fixture_list_params, list)
 
     def test_list_one(self, fixture_list_params):
-        pass
+        list_copy = fixture_list_params.copy()
+        assert list_copy == [11, 12, 13, 14, 15]
 
     def test_list_two(self, fixture_list_params):
-        pass
+        list_copy = fixture_list_params.copy()
+        list_copy.reverse()
+        assert list_copy == [15, 14, 13, 12, 11]
 
     def test_list_three(self, fixture_list_params):
-        pass
+        list_copy = fixture_list_params.copy()
+        list_copy.clear()
+        assert list_copy == []
 
     def test_list_four(self, fixture_list_params):
-        pass
+        list_copy = fixture_list_params.copy()
+        list_copy.append(16)
+        assert list_copy == [11, 12, 13, 14, 15, 16]
 
     def test_list_five(self, fixture_list_params):
-        pass
+        list_copy = fixture_list_params.copy()
+        list_copy.remove(13)
+        assert list_copy == [11, 12, 14, 15]
 
