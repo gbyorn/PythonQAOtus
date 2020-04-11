2634e0715d2e4b586026e855c2cb9991fe523401
gbyorn <byorn.gosu@gmail.com>
2020-04-11 18:28:54 +0300

    Add tests and descriptions for string

--- HomeWork2/test_str.py
+++ HomeWork2/test_str.py
@@ -3,17 +3,19 @@ class TestString:
         assert isinstance(fixture_string_params, str)
 
     def test_str_one(self, fixture_string_params):
-        pass
+        new_string = fixture_string_params.rstrip('5')
+        assert new_string == '1234'
 
     def test_str_two(self, fixture_string_params):
-        pass
+        new_string = fixture_string_params.replace('3', '6')
+        assert new_string == '12645'
 
     def test_str_three(self, fixture_string_params):
-        pass
+        assert fixture_string_params.find('4') == 3
 
     def test_str_four(self, fixture_string_params):
-        pass
+        assert fixture_string_params.startswith('1')
 
     def test_str_five(self, fixture_string_params):
-        pass
+        assert fixture_string_params.isalnum()
 
