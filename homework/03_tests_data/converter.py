764a972d963b9be71525c7c9d4f12e62bd8d3763
gbyorn <byorn.gosu@gmail.com>
2020-04-19 13:33:32 +0300

    Fix structure for Homework3

--- homework/03_tests_data/converter.py
+++ homework/03_tests_data/converter.py
@@ -6,7 +6,7 @@ books_dict = {}
 users_dict = {}
 output_dict = {"Users": []}
 
-with open("../data_input/input_csv_file_hw3.csv") as input_csv:
+with open("../../data_input/input_csv_file_hw3.csv") as input_csv:
     reader = csv.DictReader(input_csv)
     for count, row in enumerate(reader, 1):
         books_dict[count] = {
@@ -14,7 +14,7 @@ with open("../data_input/input_csv_file_hw3.csv") as input_csv:
             "author": row["Author"],
             "height": row["Height"]}
 
-with open("../data_input/input_json_file_hw3.json") as input_json:
+with open("../../data_input/input_json_file_hw3.json") as input_json:
     users = json.loads(input_json.read())
     for count, user in enumerate(users, 1):
         users_dict[count] = {
@@ -31,6 +31,6 @@ for number, user in users_dict.items():
         books_dict.pop(number)
     output_dict["Users"].append(temp_user)
 
-with open("../data_output/output_json_file_hw3.json", "w") as output_json:
+with open("../../data_output/output_json_file_hw3.json", "w") as output_json:
     s = json.dumps(output_dict, indent=4)
     output_json.write(s)
